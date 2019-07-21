f8faf8328b713e05b8047053301961ae0c3e756e
keymoon <kymn0116@gmail.com>
2019-07-22 03:25:28 +0900

    [add] entry point

---
+++ src/main.js
@@ -0,0 +1,10 @@
+// ==UserScript==
+// @name        ac-favorite-manager
+// @namespace   https://atcoder.jp/
+// @version     0.0.1
+// @description AtCoderのお気に入りの管理を行います。
+// @author      keymoon
+// @license     MIT
+// @match       https://atcoder.jp/*
+// @exclude     https://atcoder.jp/*/json
+// ==/UserScript==
