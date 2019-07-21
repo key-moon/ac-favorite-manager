5af5fd10fb51921b46d30ecd2ef2aace3591b27e
keymoon <kymn0116@gmail.com>
2019-07-22 03:31:30 +0900

    [add] webpack config

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+﻿const path = require("path");
+
+module.exports = {
+    mode: "production",
+    entry: path.resolve(__dirname, "src/main.js"),
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(html)$/,
+                use: ["html-loader"]
+            }
+        ]
+    },
+    externals: {
+        jquery: "jQuery"
+    },
+    optimization: {
+        minimize: false
+    }
+};
